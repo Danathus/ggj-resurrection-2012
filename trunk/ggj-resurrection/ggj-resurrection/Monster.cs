c8f3ec4d7a2ded738e275fd50472dfe70d3e7b78
larrylebron@gmail.com <larrylebron@gmail.com@8e592a86-c445-7b74-4ca5-882a05fab76b>
2012-01-28 06:46:32 +0000

    Added spritebatch to draw, added loadData to all classes

--- trunk/ggj-resurrection/ggj-resurrection/Monster.cs
+++ trunk/ggj-resurrection/ggj-resurrection/Monster.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace ggj_resurrection
 {
     class Monster : GameObject
     {
-        public override void Draw()
+
+
+        public override void Draw(SpriteBatch SpriteBatch)
         {
+                   
+           //mSpriteBatch.Draw(mTexture, mPosition, Color.YellowGreen);
+
         }
 
         public override void Update(GameTime gameTime)
         {
+ 
+        }
+
+        public override void LoadData(Game myGame)
+        {
+
         }
     }
 }
