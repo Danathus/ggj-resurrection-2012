e8db67582666659369138924d058e16e539cc6d8
Danathus <Danathus@8e592a86-c445-7b74-4ca5-882a05fab76b>
2012-01-28 06:33:33 +0000

    now Monster and Player implement an interface defined in GameObject

--- trunk/ggj-resurrection/ggj-resurrection/Player.cs
+++ trunk/ggj-resurrection/ggj-resurrection/Player.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace ggj_resurrection
 {
-    class Player : GameObject
+    public class Player : GameObject
     {
+        public override void Draw()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
     }
 }
