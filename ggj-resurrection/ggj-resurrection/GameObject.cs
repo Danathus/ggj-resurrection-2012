e56f73fd53102738a2a18cd987e88810e794d7b1
larrylebron <larrylebron@gmail.com>
2012-01-28 06:46:32 +0000

    Added spritebatch to draw, added loadData to all classes

--- ggj-resurrection/ggj-resurrection/GameObject.cs
+++ ggj-resurrection/ggj-resurrection/GameObject.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace ggj_resurrection
 {
     public abstract class GameObject
     {
-        public abstract void Draw();
+        public abstract void Draw(SpriteBatch SpriteBatch);
         public abstract void Update(GameTime gameTime);
+        public abstract void LoadData(Game myGame);
     };
 }
--- ggj-resurrection/ggj-resurrection/Player.cs
+++ ggj-resurrection/ggj-resurrection/Player.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace ggj_resurrection
 {
     public class Player : GameObject
     {
-        public override void Draw()
+        public override void Draw(SpriteBatch SpriteBatch)
         {
         }
 
         public override void Update(GameTime gameTime)
         {
         }
+
+        public override void LoadData(Game myGame)
+        {
+
+        }
+       
     }
 }
