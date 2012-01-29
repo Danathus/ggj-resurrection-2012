90eb5e224b0b814d89f1f7d3f2bb048130b5ba29
mattcollinsggj <mattcollinsggj@gmail.com>
2012-01-29 01:22:32 +0000

    BAAAAAAAAAATS

--- ggj-resurrection/ggj-resurrection/SwordSlash.cs
+++ ggj-resurrection/ggj-resurrection/SwordSlash.cs
@@ -20,7 +20,11 @@ namespace ggj_resurrection
     {
         static private Texture2D mTexture;
 
+        int opacity = 255;
+        bool timedOut = false;
         float mSlashTimeout;
+        Vector2 rightStickRotation;
+        float batRotation;
         const float mMaxSlashTimeout = 0.5f;
 
         public SwordSlash(World world, Vector2 initPos)
@@ -29,13 +33,14 @@ namespace ggj_resurrection
             mSlashTimeout = mMaxSlashTimeout;
             mRadius = 1;
 
-            mBody = BodyFactory.CreateCircle(mPhysicsWorld, mRadius, 1f, mPosition);
+            mBody = BodyFactory.CreateRectangle(mPhysicsWorld, mTexture.Width / 64f, mTexture.Height / 64f, 1f);
             mBody.BodyType = BodyType.Dynamic;
 
             mBody.CollisionCategories = Category.Cat2;
             mBody.UserData = "Sword";
             
-            mFixture = FixtureFactory.AttachCircle(mRadius, 1f, mBody);
+            mFixture = FixtureFactory.AttachRectangle(mTexture.Width / 64f, mTexture.Height / 64f, 1f, new Vector2(mTexture.Width / 2, mTexture.Height / 2), mBody);
+            mBody.Position = mPosition / 64f;
             mFixture.CollisionCategories = Category.Cat2;
             mFixture.Body.CollisionCategories = Category.Cat2;
             mFixture.CollidesWith = Category.All & ~Category.Cat1;
@@ -55,14 +60,47 @@ namespace ggj_resurrection
             return true;
         }
 
+        public bool isTimedOut()
+        {
+            return timedOut;
+        }
+
         public override void Draw(SpriteBatch spriteBatch)
         {
             if (mSlashTimeout > 0)
             {
                 //spriteBatch.Draw(mTexture, mPosition, new Color(255, 255, 255, mSlashTimeout / mMaxSlashTimeout * 255));
-                spriteBatch.Draw(mTexture, mBody.Position, null, new Color(255, 255, 255, mSlashTimeout / mMaxSlashTimeout * 255),
-                                0f, new Vector2(mTexture.Width / 2, mTexture.Height / 2), Camera.kPixelsToUnits, SpriteEffects.None, 0f);
+                spriteBatch.Draw(mTexture, mBody.Position * 64f, null, new Color(255, 255, 255, opacity),
+                                batRotation, new Vector2(mTexture.Width / 2, mTexture.Height / 2), 1f, SpriteEffects.None, 0f);
             }
+        }
+
+        public void setAngularVelocity(SwordSlash old, SwordSlash current)
+        {
+            float difference = current.batRotation - old.batRotation;
+            difference /= 5000f;
+            mFixture.Body.AngularVelocity = difference;
+        }
+
+        public void setRotation(Vector2 stick)
+        {
+            rightStickRotation = stick;
+
+            //rightStickRotation.Normalize();
+            double ratio = rightStickRotation.Y / rightStickRotation.X;
+            float rotator = (float)Math.Atan(ratio);
+            
+
+            //rotator *= (3.1415926535f / 180f);
+            rotator += ((3f *3.1415926535f )/ 2f);
+
+            if (rightStickRotation.X < 0)
+            {
+                rotator += (3.1415926535f);
+            }
+
+            batRotation = rotator;
+
         }
 
         public override void Update(GameTime gameTime)
@@ -73,14 +111,18 @@ namespace ggj_resurrection
             if (mSlashTimeout < 0.0f)
             {
                 mSlashTimeout = 0.0f;
+                timedOut = true;
                 // remove thyself
                 GetGameWorld().RemoveGameObject(this);
             }
+
+            opacity -= 5;
+
         }
 
         public static void LoadData(Game myGame)
         {
-            mTexture = myGame.Content.Load<Texture2D>("enemySprites/monster");
+            mTexture = myGame.Content.Load<Texture2D>("Bat");
         }
     }
 }
