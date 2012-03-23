4ea5eb297fd7c7d4e1123171a6b10c854766fcc7
Danathus <Danathus@8e592a86-c445-7b74-4ca5-882a05fab76b>
2012-03-23 09:22:29 +0000

    GBR C++ implementation now builds with SDL. Note: You will want to have SDL installed and have a SDLDIR env var pointing to the directory.

--- trunk/cplusplus_implementation/GroundBeefResurrection/src/main.cpp
+++ trunk/cplusplus_implementation/GroundBeefResurrection/src/main.cpp
@@ -1,7 +1,17 @@
+#include <SDL.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1)
+	{
+		printf("Could not initialize SDL: %s.\n", SDL_GetError());
+		exit(-1);
+	}
+
 	printf("hello world!\n");
+
+	SDL_Quit();
+
 	return 0;
 }
