c73e4bbf622705732a2b25078dbc43f05add522c
Danathus <Danathus@users.noreply.github.com>
2012-03-23 09:32:30 +0000

    augmented C++ code a bit -- now we create a window and spin in a loop until we get a quit message

--- cplusplus_implementation/GroundBeefResurrection/src/main.cpp
+++ cplusplus_implementation/GroundBeefResurrection/src/main.cpp
@@ -3,14 +3,51 @@
 
 int main(int argc, char *argv[])
 {
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1)
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf("Could not initialize SDL: %s.\n", SDL_GetError());
+		fprintf(stderr, "Could not initialize SDL: %s.\n", SDL_GetError());
 		exit(-1);
 	}
 
-	printf("hello world!\n");
+	// query video information
+	const SDL_VideoInfo *info = SDL_GetVideoInfo();
+	if (!info)
+	{
+		fprintf(stderr, "Failed to get video info: %s.\n", SDL_GetError());
+		exit(1);
+	}
 
+	// set display parameters
+	int width  = 640;
+	int height = 480;
+	int bpp    = info->vfmt->BitsPerPixel; // bits per pixel
+	int flags  = SDL_OPENGL;
+
+	// set up our initial display
+	if (SDL_SetVideoMode(width, height, bpp, flags) == 0)
+	{
+		// error
+		fprintf(stderr, "Failed to set video mode: %s.\n", SDL_GetError());
+		exit(-1);
+	}
+
+	// main loop
+	bool done = false;
+	while (!done)
+	{
+		// process SDL events
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) switch (event.type)
+		{
+		case SDL_KEYDOWN:
+			break;
+		case SDL_QUIT:
+			done = true;
+			break;
+		}
+	}
+
+	// finish up
 	SDL_Quit();
 
 	return 0;
